e16d5f83be3a43e3d751a8388f5f2bfd101521f0
OaklandHacksAdminUser <hello.ohacks@gmail.com>
2017-03-04 17:55:05 -0800

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -473,16 +473,14 @@
     }
     });
 
-})(jQuery);
-!function() {
-    try {
-        document.getElementsByClassName('engine')[0].getElementsByTagName('a')[0].removeAttribute('rel');
-    } catch(err){ }
-    if(!document.getElementById('top-1')) {
-        var e = document.createElement("section");
-        e.id = "top-1";
-        e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.10.5';
-        document.body.insertBefore(e, document.body.childNodes[0]);
-    }
-}();
+    // Fix menu for Opera Mini and Android Browsers < 4.4v
+    if(navigator.userAgent.match(/(Opera Mini)|(534\.30)|(534\.13)|(530\.17)|(533\.1)/i )){ 
+        if($('nav.navbar').length){
+            var color = $('nav.navbar .nav-link').css('color') || '#c8c8c8';
+            $('.navbar-toggler .hamburger-icon').remove();
+            $('.navbar-toggler:eq(0)').addClass('collapsed').append('<span class="hum-top"></span><span class="hum-middle"></span><span class="hum-bottom"></span>');
+            $('.navbar-toggler span').not('.close-icon').css('background-color', color);
+        }
+    }    
+
+})(jQuery);!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.12.1';document.body.insertBefore(a,document.body.childNodes[0])}}();
